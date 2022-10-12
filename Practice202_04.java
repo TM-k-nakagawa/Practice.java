436dc38cdf04e3318a0e7f4214b66b54d05c2c59
TM-k-nakagawa <k-nakagawa@tech-mart.co.jp>
2022-10-12 14:46:00 +0900

    202＿04コンパイル成功

---
+++ Practice202_04.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Practice202_04 {
+  public static void main(String[] args){
+  Scanner scanner = new Scanner(System.in);  
+  System.out.println("あなたの名前を入力して下さい");
+  String name = scanner.next();
+  System.out.println( name +"さんこんにちは。");
+  System.out.println("あなたは何歳ですか？");
+  String old = scanner.next();
+  System.out.println(old +"歳なんですね。よろしくお願いします");
+  scanner.close();
+  } 
+}
