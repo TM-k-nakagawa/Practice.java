b6df9780af523a4a76229b07bcead70905f5764a
TM-k-nakagawa <k-nakagawa@tech-mart.co.jp>
2022-10-12 17:36:29 +0900

    203_02を無理やり書いてコンパイルした

--- Practice203_01.java
+++ Practice203_01.java
@@ -2,8 +2,8 @@ public class Practice203_01 {
   public static void main(String[] args) {
   int number = 7;
   int count = 100/number;
-  for (int i = 1; i < count + 1 ; i++){
-  System.out.println(number*i);
+    for (int i = 1; i < count + 1 ; i++){
+      System.out.println(number*i);
+    }
   }
-}
 }
