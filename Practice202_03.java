6e09037adc6244190af567890fdd3c46e356d15c
TM-k-nakagawa <k-nakagawa@tech-mart.co.jp>
2022-10-12 14:22:57 +0900

    202_03コンパイル成功

---
+++ Practice202_03.java
@@ -0,0 +1,13 @@
+public class Practice202_03 {
+  public static void main(String args[]){
+  String a = "1234567890";
+  String b = "１２３４５６７８９０";
+  String c = "飛べない豚は、ただの豚だ。";
+  int num_a = a.length();
+  int num_b = b.length();
+  int num_c = c.length();
+  System.out.println("1行目の文字数は" + num_a);
+  System.out.println("2行目の文字数は" + num_b);
+  System.out.println("3行目の文字数は" + num_c);
+  }  
+}
