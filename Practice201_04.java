eb3097b67853fa8cf0495146dfb54563e65314c4
TM-k-nakagawa <k-nakagawa@tech-mart.co.jp>
2022-10-12 13:12:53 +0900

    201_04コンパイル成功

--- Practice201_04.java
+++ Practice201_04.java
@@ -1,7 +1,12 @@
+import java.math.BigDecimal;
+
 public class Practice201_04 {
   public static void main(String[] args) {
-  double a = 1.1;
-    System.out.println( a + a + a ) 
-    System.out.println(a * a * a)
+    BigDecimal a = new BigDecimal("1.1");
+    BigDecimal b = new BigDecimal("1.1");
+    BigDecimal result1 = b.add(a).add(a);
+    BigDecimal result2 = b.multiply(a).multiply(a);
+    System.out.println( a +"+"+ a +"+" + a + "=" + result1);
+    System.out.println( a +"x"+ a +"x"+ a  + "=" + result2);
   }
 }
