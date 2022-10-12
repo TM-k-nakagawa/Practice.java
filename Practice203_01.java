aad0623381842c6614bcf2626061ad34b3b1fbf3
TM-k-nakagawa <k-nakagawa@tech-mart.co.jp>
2022-10-12 16:19:28 +0900

    203_01コンパイル成功

---
+++ Practice203_01.java
@@ -0,0 +1,9 @@
+public class Practice203_01 {
+  public static void main(String[] args) {
+  int number = 7;
+  int count = 100/number;
+  for (int i = 1; i < count + 1 ; i++){
+  System.out.println(number*i);
+  }
+}
+}
