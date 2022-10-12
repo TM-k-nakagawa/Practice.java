91fac5862050b1e4842f0f4244e7d0ecd83f39b7
TM-k-nakagawa <k-nakagawa@tech-mart.co.jp>
2022-10-12 11:23:30 +0900

    201完成

--- Practice201.java
+++ Practice201.java
@@ -1,1 +1,18 @@
-こんにちは
+public class Practice201 {
+  public static void main(String[] args) {
+    int a = 100; 
+    double b = 100.12;
+    String c = "'100'"; 
+    String d = "\"100\"";
+    String e = "ヤッホー";
+    boolean f = true;
+    boolean g = false;
+  System.out.println(a);
+  System.out.println(b);
+  System.out.println(c);
+  System.out.println(d);
+  System.out.println(e);
+  System.out.println(f);
+  System.out.println(g);
+  }
+}
