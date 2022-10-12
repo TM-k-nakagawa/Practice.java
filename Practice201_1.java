6ce8390f08a7636f41ac18363c5d9cb559cdc7d2
TM-k-nakagawa <k-nakagawa@tech-mart.co.jp>
2022-10-12 15:59:11 +0900

    ファイル名の変更

---
+++ Practice201_1.java
@@ -0,0 +1,18 @@
+public class Practice201_1 {
+  public static void main(String[] args) {
+    int a = 100; 
+    double b = 100.12;
+    String c = "'100'"; 
+    String d = "\"100\"";
+    String e = "ヤッホー";
+    boolean f = true;
+    boolean g = false;
+  System.out.println(a);
+  System.out.println(b);
+  System.out.println(c);
+  System.out.println(d);
+  System.out.println(e);
+  System.out.println(f);
+  System.out.println(g);
+  }
+}
---
+++ Practice201_2.java
@@ -0,0 +1,7 @@
+public class Practice201_2 {
+  public static void main(String[] args) {
+    int a = 100;
+    int b = 3;
+  System.out.println(a * b);
+  }
+}
