d1edd7c412600a07738c144f844fe9a1fb6b180a
TM-k-nakagawa <k-nakagawa@tech-mart.co.jp>
2022-10-14 10:55:09 +0900

    204_01できました。

---
+++ Practice204_01.java
@@ -0,0 +1,7 @@
+public class Practice204_01 {
+    public static void main(String[] args){
+    //文字列入れる
+    String text = "タイトル: なんとなく\r\n  私は生まれ変わったら\"くも\"になりたい。\r\n  虫の\'くも\'ではなく空に浮かぶ\"くも\"。";
+    System.out.println(text);
+    }
+}
