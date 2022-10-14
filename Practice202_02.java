47d9b7b67bf52438c3fdb80bd11e1ba068ae2497
TM-k-nakagawa <k-nakagawa@tech-mart.co.jp>
2022-10-14 13:21:36 +0900

    202修正

--- Practice202_02.java
+++ Practice202_02.java
@@ -1,8 +1,8 @@
 public class Practice202_02 {
-  public static void main(String[] args){
-  int a = 100;
-  int b = 300;
-  int answer = a * 25 + b * 25;
-  System.out.println(a +"円のバナナを25個、" + b +"円の桃を25個買ったら、合計" + String.format("%,d", answer) + "円になりました。");
-  }    
+    public static void main(String[] args){
+    int a = 100;
+    int b = 300;
+    int answer = a * 25 + b * 25;
+    System.out.println(a +"円のバナナを25個、" + b +"円の桃を25個買ったら、合計" + String.format("%,d", answer) + "円になりました。");
+    }    
 }
--- Practice202_03.java
+++ Practice202_03.java
@@ -1,13 +1,13 @@
 public class Practice202_03 {
-  public static void main(String args[]){
-  String a = "1234567890";
-  String b = "１２３４５６７８９０";
-  String c = "飛べない豚は、ただの豚だ。";
-  int numA = a.length();
-  int numB = b.length();
-  int numC = c.length();
-  System.out.println("1行目の文字数は" + numA);
-  System.out.println("2行目の文字数は" + numB);
-  System.out.println("3行目の文字数は" + numC);
-  }  
+    public static void main(String args[]){
+    String a = "1234567890";
+    String b = "１２３４５６７８９０";
+    String c = "飛べない豚は、ただの豚だ。";
+    int numA = a.length();
+    int numB = b.length();
+    int numC = c.length();
+    System.out.println("1行目の文字数は" + numA);
+    System.out.println("2行目の文字数は" + numB);
+    System.out.println("3行目の文字数は" + numC);
+    }  
 }
