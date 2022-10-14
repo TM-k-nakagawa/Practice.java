bf41fc3b0be01af84490c22efd317c081c97dad1
TM-k-nakagawa <k-nakagawa@tech-mart.co.jp>
2022-10-14 12:08:27 +0900

    203たち修正

--- Practice203_02_01.java
+++ Practice203_02_01.java
@@ -1,12 +1,12 @@
 public class Practice203_02_01 {
-  public static void main(String[] args){
-  int Count = 1;
-  int FirstSum = 0; 
-  int SecondSum = 0;
-    for(; Count < 100; Count = Count + 2 ){
-      FirstSum = SecondSum + Count;
-      SecondSum = FirstSum;
-      System.out.println(SecondSum + "+" + Count + "="+ FirstSum);
-    }
-  }  
+    public static void main(String[] args){
+        int Count = 1;
+        int FirstSum = 0; 
+        int SecondSum = 0;
+        for(; Count < 100; Count = Count + 2 ){
+            FirstSum = SecondSum + Count;
+            SecondSum = FirstSum;
+            System.out.println(SecondSum + "+" + Count + "="+ FirstSum);
+        }
+    }  
 }
